ff7fb88b9d2f00832f71fc5935a4cb430c7c8a15
Kadaryan <changeword@hotmail.com>
2016-11-28 01:25:21 -0300

    Update tablas.php

--- tablas.php
+++ tablas.php
@@ -9,7 +9,6 @@ while ($row = mysqli_fetch_array($tablas)){
     <form action='delete.php' method='POST'>
     <td class='tdId'><input type='text' value='".$row['id']."' name='id' class='idInput'/></td>
     <td>".$row['titulo']."</td>
-    <input type='hidden' value='anillos' name='tabla'/>
     <td><input type='submit' name='delete' class='btn-delete' value='Eliminar'/></td>
     </form>
   </tr>
